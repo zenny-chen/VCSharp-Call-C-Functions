c2cd64d2789f0059214bc9129e53ef8180bfb926
Zenny Chen <zenny_chen@sina.com>
2019-12-03 18:43:12 +0800

    Add files via upload

--- CSharpInvokeNativeCFunction/CSharpInvokeNativeCFunction/Program.cs
+++ CSharpInvokeNativeCFunction/CSharpInvokeNativeCFunction/Program.cs
@@ -13,6 +13,43 @@ namespace CSharpInvokeNativeCFunction
         [DllImport("MyDLL.dll", EntryPoint = "MyNativeModifyString", CharSet = CharSet.Unicode)]
         private static extern bool MyNativeModifyString(int outputMaxlen, StringBuilder outputStr, IntPtr pOutStrLen);
 
+        /// <summary>
+        /// 声明一个委托，用于将指定类方法传递给本地C语言函数进行回调
+        /// </summary>
+        /// <param name="array">指向一个整型数组</param>
+        /// <param name="length">指定整型数组的长度</param>
+        private delegate void ArrayFuncDelegate(IntPtr array, int length);
+
+        [DllImport("MyDLL.dll", EntryPoint = "RegisterCallbackMethods")]
+        private static extern void RegisterCallbackMethods(ArrayFuncDelegate csharpMethod);
+
+
+        [DllImport("MyDLL.dll", EntryPoint = "TestCSharpMethodCallback")]
+        private static extern void TestCSharpMethodCallback();
+
+        /// <summary>
+        /// 这里全局初始化类属性theFuncDelegate，使得它能被一直hold住，而不被释放
+        /// </summary>
+        private static ArrayFuncDelegate theFuncDelegate = new ArrayFuncDelegate(CSharpMethod);
+
+        /// <summary>
+        /// 将在本地C语言端进行回调的C#类方法
+        /// </summary>
+        /// <param name="array">指向一个整型数组</param>
+        /// <param name="length">指定整型数组的长度</param>
+        private static void CSharpMethod(IntPtr array, int length)
+        {
+            int sum = 0;
+            unsafe
+            {
+                var arr = (int*)array.ToPointer();
+                for (int i = 0; i < length; i++)
+                    sum += arr[i];
+            }
+
+            Console.WriteLine("In the C# method, the sum is: " + sum);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -52,6 +89,15 @@ namespace CSharpInvokeNativeCFunction
                 Console.WriteLine("MyNativeModifyString calling failed!!");
 
             outputStrLenPtr.Free();
+
+            Console.WriteLine("\n");
+
+            // 测试从本地C语言端回调C#的类方法
+            // 首先需要将C#端要被回调的类方法注册给本地C语言端
+            RegisterCallbackMethods(theFuncDelegate);
+
+            // 然后，调用本地C语言端的测试函数，它将会回调自己所注册的C#端的类方法
+            TestCSharpMethodCallback();
         }
     }
 }
